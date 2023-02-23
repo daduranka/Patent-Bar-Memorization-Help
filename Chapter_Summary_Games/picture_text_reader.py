0e9ca1647b7516594229c705997fc7558cf936f2
daduranka <116219580+daduranka@users.noreply.github.com>
2023-02-22 21:24:10 -0700

    converting pictures to text

---
+++ Chapter_Summary_Games/picture_text_reader.py
@@ -0,0 +1,31 @@
+
+import cv2
+import pytesseract
+import os
+
+directory = "C:/Users/dadur/Documents/PatentBarPrep"
+
+for subdirectories, directories, files in os.walk(directory):
+    if(subdirectories.startswith('C:/Users/dadur/Documents/PatentBarPrep\M')):
+        #print(subdirectories[39:])
+        for f in files:
+            # Load the image file
+            img = cv2.imread(f"C:/Users/dadur/Documents/PatentBarPrep/{subdirectories[39:]}/{f}")
+
+            # Preprocess the image
+            try:
+                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                
+                #Selecting download directory for pytesseract
+                pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
+
+                #Extract text using Pytesseract
+                text = pytesseract.image_to_string(gray)
+
+                #Print the extracted text
+                with open(f'C:/Users/dadur/Documents/PatentBarPrep/{subdirectories[39:]}/{f[:-3]}txt', 'w') as file: 
+                    file.write(text)
+            except:
+                print(f"C:/Users/dadur/Documents/PatentBarPrep/{subdirectories[39:]}/{f} failed!")
+                
+
